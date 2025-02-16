5c7c289def732d170a859acc373bae8d1e1fe4d3
Michele Marotto <michele@monkeylabs.nl>
2025-02-16 14:31:10 +0100

    use webgl

--- src/screenshot.js
+++ src/screenshot.js
@@ -4,7 +4,7 @@ import { GRAPH_URL } from './config.js';
 export async function captureChart(symbol = 'AAPL', interval = '1D') {
     const browser = await chromium.launch({
         headless: true,
-        args: ['--use-gl=egl']
+        args: ['--enable-webgl', '--ignore-gpu-blacklist']
     });
     const page = await browser.newPage();
     await page.setViewportSize({
